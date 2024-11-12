8cf27d3bd6307c4cd4db9b631b96e268d350c894
APPPRAXICAN <unknownworlds990@gmail.com>
2024-11-12 04:31:44 +0200

    finishing particapnts controller

--- app/Http/Controllers/ParticapantController.php
+++ app/Http/Controllers/ParticapantController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ParticapantController extends Controller
 {
     public function subscribe(Request $r)
     {
-        //
+        $r->validate([
+            'code' => 'required|min:10',
+        ]);
+        Room::find($r->code)->participants()->attach(Auth::id());
     }
     public function unsubscribe(Request $r)
     {
-        //
+        $r->validate([
+            'code' => 'required|min:10',
+        ]);
+        Room::find($r->code)->participants()->detach(Auth::id());
     }
 }
