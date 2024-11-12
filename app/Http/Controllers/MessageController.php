d41d5d999e440f8176ad9355b16552ad03ef0700
APPPRAXICAN <unknownworlds990@gmail.com>
2024-11-12 03:47:53 +0200

    crud

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Broadcast;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index($room_id)
+    {
+        return view('', Message::latest()->where('room_id', $room_id)->get());
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'text' => 'required|min:3|max:100',
+        ]);
+        $message = Message::create([
+            'text' => $request->text,
+            'user_id' => Auth::id(),
+        ]);
+        Broadcast(new MessageController($message));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Message::find($id)->delete();
+    }
+}
---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class RoomController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('', ['rooms' => Room::latest()->where('participant_id', Auth::id())->get()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {}
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|min:5',
+        ]);
+        Room::created([
+            'admin_id' => Auth::id(),
+            'title' => $request->title,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return view('', ['room' => Room::find($id)->first()]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        return view('');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $request->validate([
+            'title' => 'required|min:5',
+        ]);
+        $room = Room::find($id)->frist();
+        $room->update($data);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Room::find($id)->destroy();
+    }
+}
