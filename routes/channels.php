b3464830e3bb21b619dde114f498d566e266ce66
APPPRAXICAN <unknownworlds990@gmail.com>
2024-11-12 04:32:08 +0200

    authorize message channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\Room;
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+
+Broadcast::channel('message.{room_id}', function (User $user, $room_id) {
+    // if null do not broadcast
+    return Room::find($room_id)->participants->where('id', $user->id)->first();
 });
